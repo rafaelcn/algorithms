f8dc1b4636f480ebbb40f334504e1f1b24900692
Rafael Campos Nunes <rafaelnunes@engineer.com>
2017-10-14 05:59:21 -0300

    Adds functionality fo the main function
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/uni/encadeamento/main.c
+++ src/uni/encadeamento/main.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "matrix.h"
+#include <string.h>
 
 // Would be best to filter by compiler and not by platform as it isn't assured
 // that the define is implemented in the respective compiler.
 #ifdef __windows__
-    #include <locale.h>
+#include <locale.h>
 #endif // __windows__
+
+#include "matrix.h"
+
+// Prints usage about the program
+void usage();
 
 int main(int argc, char **argv) {
 #ifdef __windows__
     // Little fix for windows shitty console (I'm looking at you CMD.exe).
     set_locale(LC_ALL, "");
-#endif
+#endif // __windows__
 
-    // TODO: Interpret input and make a menu to interact with the matrix
+    int x = 3;
+    int y = 3;
 
+// TODO: Interpret input and make a menu to interact with the matrix
+    if (argc > 1) {
+        if (strcmp(argv[1], "--help") == 0) {
+            usage();
+            return 0;
+        }
 
-    Matrix *m = matrix_init(5, 3);
+        x = atoi(argv[1]);
+        y = atoi(argv[2]);
+    }
+
+    Matrix *m = matrix_init(x, y);
 
     matrix_print(*m);
 
+    matrix_free(*m);
+
     return 0;
+}
+
+void usage() {
+    printf("\nUsage: ./matrix <x> <y>");
+    printf("\n\nWhere: ");
+    printf("\nx - Rows of the matrix");
+    printf("\ny - Columns of the matrix");
 }
