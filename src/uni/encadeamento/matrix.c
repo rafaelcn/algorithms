fdd0c64ccfabbc7d243696ddd4132c2d8700fcbd
Rafael Campos Nunes <rafaelnunes@engineer.com>
2017-10-14 03:24:43 -0300

    Implements the init function and other functions
        Though not complete yet, at least they don't give you shitty
    SEGFAULT on your face.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/uni/encadeamento/matrix.c
+++ src/uni/encadeamento/matrix.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <stdint.h>
+#include <inttypes.h>
 
 #include "matrix.h"
 
@@ -12,10 +12,10 @@ struct node {
     uint32_t pos_x;
     uint32_t pos_y;
 
-    struct node *top;
-    struct node *bottom;
-    struct node *left;
-    struct node *right;
+    MatrixNode *top;
+    MatrixNode *bottom;
+    MatrixNode *left;
+    MatrixNode *right;
 };
 
 /**
@@ -34,12 +34,38 @@ static int check_pointer(void *p) {
  * @param m The message to be printed.
  * @param line The line where the error was issued.
  */
-static void pferror(char *m, int line) {
+static void pferror(const char *m, const int line) {
 #ifdef __linux__
-    fprintf(stderr, "\nError %s | matrix.c on line %d\n", m, line);
+    fprintf(stderr, "\n%sError: %s | matrix.c on line %d%s\n", ANSI_COLOR_RED,
+            m, line, ANSI_COLOR_RESET);
 #elif
-    fprintf(stderr, "\nError %s | matrix.c on line %d\n", m, line);
+    fprintf(stderr, "\nError: %s | matrix.c on line %d\n", m, line);
 #endif // __linux__
+}
+
+/**
+ * @brief Prints a formatted info.
+ * @param m The message to be printed.
+ */
+static void pfinfo(const char *m) {
+#ifdef __linux__
+    fprintf(stderr, "\n%sInfo: %s%s", ANSI_COLOR_GREEN, m, ANSI_COLOR_RESET);
+#elif
+    fprintf(stderr, "\nInfo: %s", m);
+#endif // __linux__
+}
+
+/**
+ * @brief Recursive function that prints information about each head of the
+ * Matrix.
+ */
+static void print_heads(MatrixNode *mn) {
+    if (mn == NULL) {
+        return;
+    }
+
+    printf("\n\t--(%d)--\n\t%4s", mn->value, "|");
+    print_heads(mn->bottom);
 }
 
 Matrix *matrix_init(int x, int y) {
@@ -50,10 +76,15 @@ Matrix *matrix_init(int x, int y) {
         exit(1);
     }
 
-    // TODO: Create the Matrix if the allocation succeeds
-    // The process of creating the Matrix is create a double linked list and
-    // for each pair of lists concatenate (link) those sublist together.
+    // The process of creating the Matrix is create all the lines of the list
+    // first and then create a double linked list and for each pair of lists
+    // concatenate (link) those sublist together.
 
+
+    // Walks along the HEADs.
+    MatrixNode* walker = NULL;
+
+    // Create every HEAD
     for (int i = 0; i < x; i++) {
         // Creating the HEAD node of the Matrix
         MatrixNode *HEAD = malloc(sizeof(MatrixNode));
@@ -63,40 +94,103 @@ Matrix *matrix_init(int x, int y) {
             exit(1);
         }
 
-        HEAD->top = HEAD->bottom = NULL;
-        HEAD->left = HEAD->right = NULL;
-
         // Setting its position on the Matrix
         HEAD->pos_x = i;
         // The HEAD element is always the first of its column
         HEAD->pos_y = 0;
 
-        *m = HEAD;
-        Matrix pointer = HEAD;
+        // Only for testing purposes
+        HEAD->value = i;
+
+        if (*m == NULL) { // Empty Matrix
+            HEAD->top = HEAD->bottom = NULL;
+            *m = HEAD;
+        } else {
+            // The walker is ...
+            walker->top = HEAD;
+            HEAD->bottom = walker;
+        }
+
+        HEAD->left = HEAD->right = NULL;
+
+        walker = HEAD;
+
+#ifdef DEBUG
+        const char *message = "Creating HEAD %d. Walker is %p";
+        int size = snprintf(NULL, 0, message, i, walker->bottom);
+        char buffer[size+1];
+        snprintf(buffer, size, message, i, walker->bottom);
+        pfinfo(buffer);
+#endif // DEBUG
+    }
+
+    //print_heads(walker);
+
+    for (int i = 0; i < x; i++) {
+        MatrixNode* HEAD = NULL;
+        if (i == 0) {
+            // points to the first HEAD available
+            HEAD = walker;
+        } else {
+            if (walker->bottom != NULL) {
+                HEAD = walker->bottom;
+                walker = walker->bottom;
+            }
+
+            HEAD = walker;
+        }
 
         for (int j = 1; j < y; j++) {
+            MatrixNode *e = malloc(sizeof(MatrixNode));
 
-            MatrixNode *el = malloc(sizeof(MatrixNode));
-
-            if (check_pointer(el)) {
+            if (check_pointer(e)) {
                 pferror("Creating a MatrixNode failed, no memory?", __LINE__);
                 exit(1);
             }
 
-            el->left = HEAD;
-            el->right = NULL;
-            el->bottom = el->top = NULL;
-            el->pos_x = i;
-            el->pos_y = j;
+            e->left = HEAD;
+            e->right = NULL;
+            e->bottom = e->top = NULL;
+            // not sure if the below will work well
+            e->pos_x = i+1;
+            e->pos_y = j;
+            e->value = i+j;
 
-            HEAD->right = el;
+            HEAD->right = e;
 
+            HEAD = e;
         }
-
-
-
     }
 
+    matrix_print(*m);
 
     return m;
+}
+
+
+int matrix_print(Matrix m) {
+    MatrixNode *walker = m;
+
+    while(walker != NULL) {
+        while (walker->right != NULL) {
+            printf("\n\t--(%d)--\n\t%4s", walker->value, "|");
+            walker = walker->right;
+        }
+        walker = walker->bottom;
+    }
+
+    return 1;
+}
+
+
+void matrix_free(Matrix *m) {
+    //The free method is described in the Free section of the paper.
+    MatrixNode *walker = *m;
+
+    // Walker starts at 0x0 if not we have to reset its position.
+    while(walker != NULL) {
+        while (walker->right != NULL) {
+        }
+        walker = walker->bottom;
+    }
 }
--- src/uni/encadeamento/matrix.h
+++ src/uni/encadeamento/matrix.h
@@ -5,6 +5,14 @@
  *          Mikael Messias
  */
 
+// ANSI color escaping sequences
+#ifdef __linux__
+    #define ANSI_COLOR_RED     "\x1b[31m"
+    #define ANSI_COLOR_GREEN   "\x1b[32m"
+    #define ANSI_COLOR_RESET   "\x1b[0m"
+#endif // __linux__
+
+
 typedef struct node* Matrix;
 typedef struct node MatrixNode;
 
@@ -41,8 +49,16 @@ MatrixNode matrix_get_by_coordinate(Matrix *m, int x, int y);
 MatrixNode matrix_get_by_value(Matrix *m, int v);
 
 /**
+ * @brief Prints the matrix structure
+ * @param m A pointer to the Matrix to be printed.
+ * @returns 1 on success and 0 if it didn't succeed.
+ */
+int matrix_print(Matrix m);
+
+/**
  * @brief Print neighbours of an specific MatrixNode.
  * @param m The reference to be used when printing its neighbours.
+ * @returns
  */
 int matrix_print_neighbours(Matrix *m);
 
