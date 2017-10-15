716dff047b01a549c6c437dd28ef5691f1948973
Rafael Campos Nunes <rafaelnunes@engineer.com>
2017-10-15 17:42:39 -0200

    Fix position of the string printed by ASSERT
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/uni/encadeamento/assertions.h
+++ src/uni/encadeamento/assertions.h
@@ -13,17 +13,17 @@
 #define ASSERT(v, e, m)                                                 \
     printf("\n%s%s%s\n", ANSI_COLOR_YELLOW, (m), ANSI_COLOR_RESET);     \
     if ((v) != (e)) {                                                   \
-        printf("%s Got: %d. Expect: %d %s", ANSI_COLOR_RED, (v), (e),   \
+        printf("%sGot: %d. Expect: %d %s", ANSI_COLOR_RED, (v), (e),   \
                ANSI_COLOR_RESET);                                       \
     } else {                                                            \
-        printf("%s Got: %d. Expect: %d. %s", ANSI_COLOR_GREEN, (v), (e), \
+        printf("%sGot: %d. Expect: %d. %s", ANSI_COLOR_GREEN, (v), (e), \
                ANSI_COLOR_RESET);                                       \
     };
 #elif
 #define ASSERT(v, e, m)                                                 \
     printf("\n%s\n", (m));                                              \
     if ((v) != (e)) {                                                   \
-        printf("Got: %d. Expect: %d", (v), (e), );                      \
+        printf("Got: %d. Expect: %d", (v), (e));                        \
     } else {                                                            \
         printf("Got: %d. Expect: %d.", (v), (e));                       \
     };
