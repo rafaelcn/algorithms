43ff9ed5a214192022ba0c09681ee89767a5161c
Rafael Campos Nunes <rafaelnunes@engineer.com>
2017-10-15 18:10:14 -0200

    Reshape main and make it more flexibe with options
        Now the program can be run in an interactive mode, besides the
    testing mode.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/uni/encadeamento/main.c
+++ src/uni/encadeamento/main.c
@@ -9,9 +9,18 @@
 #endif // __windows__
 
 #include "matrix.h"
+#include "macros.h"
+#include "assertions.h"
 
-// Prints usage about the program
+// Prints usage about the program.
 void usage();
+// Prints an interactive Menu where you can mess with everything.
+void menu();
+// Does a testing of the matrix functions suite.
+int test(int argc, char **argv);
+// Test if a matrix is pointer is null or not.
+int check_matrix(Matrix *m);
+
 
 int main(int argc, char **argv) {
 #ifdef __windows__
@@ -19,27 +28,54 @@ int main(int argc, char **argv) {
     set_locale(LC_ALL, "");
 #endif // __windows__
 
-    int x = 3;
-    int y = 3;
-
-// TODO: Interpret input and make a menu to interact with the matrix
     if (argc > 1) {
         if (strcmp(argv[1], "--help") == 0) {
             usage();
             return 0;
+        } else if (strcmp(argv[1], "--interactive") == 0) {
+            menu();
+        } else if (strcmp(argv[1], "--test") == 0) {
+            test(argc, argv);
         }
+    } else {
+        usage();
+    };
 
-        x = atoi(argv[1]);
-        y = atoi(argv[2]);
+    return 0;
+}
+
+void usage() {
+    printf("\nUsage: ./matrix --<flag>");
+    printf("\n\nFlags available:");
+    printf("\n\t--help");
+    printf("\n\t--interactive");
+    printf("\n\t--test <x> <y>");
+    printf("\n\nWhere: ");
+    printf("\n\tx - Rows of the matrix");
+    printf("\n\ty - Columns of the matrix\n");
+}
+
+void interactive() {
+
+
+}
+
+int test(int argc, char **argv) {
+
+    int dim[2] = {3, 3};
+
+    if (argc > 3) {
+        dim[0] = atoi(argv[2]);
+        dim[1] = atoi(argv[3]);
     }
 
-    Matrix *m = matrix_init(x, y);
+    Matrix *m = matrix_init(dim[0], dim[1]);
 
     printf("\n\nTesting matrix_print\n\n");
 
     matrix_print(*m);
 
-    printf("\n\nTesting matrix_print_element\n\n");
+    printf("\n\nTesting matrix_print_element | matrix_get_by_coordinate\n\n");
 
     matrix_print_element(matrix_get_by_coordinate(*m, 1, 2));
     matrix_print_element(matrix_get_by_coordinate(*m, 2, 2));
@@ -53,17 +89,108 @@ int main(int argc, char **argv) {
     matrix_print_neighbours(matrix_get_by_coordinate(*m, 2, 1));
     matrix_print_neighbours(matrix_get_by_coordinate(*m, 1, 2));
 
+    printf("\n\nTesting: matrix_get_by_value");
+
+    matrix_print_element(matrix_get_by_value(*m, 1));
+    matrix_print_element(matrix_get_by_value(*m, 2));
+    matrix_print_element(matrix_get_by_value(*m, 3));
+
+    printf("\n\nTesting: matrix_get_value\n");
+
+    ASSERT(*(matrix_get_value(matrix_get_by_coordinate(*m, 1, 2))), 3,
+           "Testing if the value of the MatrixNode(1,2) == 3");
+
+    ASSERT(*(matrix_get_value(matrix_get_by_coordinate(*m, 1, 3))), 4,
+           "Testing if the value of the MatrixNode(1,2) == 4");
+
+    ASSERT(*(matrix_get_value(matrix_get_by_coordinate(*m, 2, 2))), 4,
+           "Testing if the value of the MatrixNode(1,2) == 4");
+
     matrix_free(*m);
 
     return 0;
 }
 
-void usage() {
-    printf("\nUsage: ./matrix <x> <y>");
-    printf("\n\nWhere: ");
-    printf("\nx - Rows of the matrix");
-    printf("\ny - Columns of the matrix");
-    printf("\n\nFlags available:");
-    printf("\n--help");
-    printf("\n--interactive");
+void menu() {
+    // The chosen option in the menu
+    int op = 0;
+
+    Matrix* m;
+    // Coordinates of the Matrix
+    int x, y;
+
+    while (op != 9){
+        printf("\n1 -> Create a Matrix\n");
+        printf("2 -> Create a MatrixNode\n");
+        printf("3 -> Get a MatrixNode by coordinate\n");
+        printf("4 -> Get a MatrixNode by value\n");
+        printf("5 -> Print the Matrix\n");
+        printf("6 -> Print the Matrix HEADS\n");
+        printf("7 -> Print the neighbours of a given MatrixNode\n");
+        printf("8 -> Remove a MatrixNode\n");
+        printf("9 -> Exit\n");
+        printf(": ");
+
+        scanf("%d", &op);
+
+        switch (op) {
+        case 1: {
+            printf("Rows: ");
+            scanf("%d", &x);
+            printf("Columns: ");
+            scanf("%d", &y);
+
+            m = matrix_init(x, y);
+            break;
+        }
+        case 2:
+            break;
+        case 3:
+            printf("\nEnter the position of the MatrixNode: ");
+            scanf("%d %d", &x, &y);
+
+            matrix_print_element(matrix_get_by_coordinate(*m, x, y));
+            break;
+        case 4: {
+            int v = 0;
+
+            printf("\nEnter the value of the MatrixNode: ");
+            scanf("%d", &v);
+
+            matrix_print_element(matrix_get_by_value(*m, v));
+            break;
+        }
+        case 5:
+            if (check_matrix(m)) {
+                matrix_print(*m);
+            }
+            break;
+        case 6:
+            if (check_matrix(m)) {
+                matrix_print_heads(*m);
+            }
+            break;
+        case 7:
+            printf("Enter the position of the MatrixNode: ");
+            scanf("%d %d", &x, &y);
+
+            matrix_print_neighbours(matrix_get_by_coordinate(*m, x, y));
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        default:
+            printf("Invalid option. Enter a valid one [1-9].\n");
+        }
+    }
+}
+
+int check_matrix(Matrix *m) {
+    if (*m != NULL) {
+        return 1;
+    } else {
+        printf("\nCreate a Matrix first.\n");
+        return 0;
+    }
 }
