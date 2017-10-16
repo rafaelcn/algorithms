239768ca70aa319c1d4acf921622fadb473f2b68
Mikael Messias <mikaelmessias@outlook.com>
2017-10-15 23:38:28 -0200

    Adds functions to insert and remove nodes from the matrix
    	It adds a function matrix_insert to insert a value into a given coordinate, and also two functions to remove nodes: matrix_remove_by_coordinate  and matrix_remove_by_value.
    
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/uni/encadeamento/matrix.c
+++ src/uni/encadeamento/matrix.c
@@ -8,16 +8,16 @@
 
 
 struct node {
-    int value;
+	int value;
 
     // Position of the node in the matrix
-    uint32_t pos_x;
-    uint32_t pos_y;
+	uint32_t pos_x;
+	uint32_t pos_y;
 
-    MatrixNode *top;
-    MatrixNode *bottom;
-    MatrixNode *left;
-    MatrixNode *right;
+	MatrixNode *top;
+	MatrixNode *bottom;
+	MatrixNode *left;
+	MatrixNode *right;
 };
 
 
@@ -32,7 +32,7 @@ static uint32_t columns;
  * 2. The number 0 representing it isn't NULL
  */
 static int check_pointer(void *p) {
-    return p == NULL;
+	return p == NULL;
 }
 
 /**
@@ -42,10 +42,10 @@ static int check_pointer(void *p) {
  */
 static void pferror(const char *m, const int line) {
 #ifdef __linux__
-    fprintf(stderr, "%sError: %s | matrix.c on line %d%s\n", ANSI_COLOR_RED,
-            m, line, ANSI_COLOR_RESET);
+	fprintf(stderr, "%sError: %s | matrix.c on line %d%s\n", ANSI_COLOR_RED,
+		m, line, ANSI_COLOR_RESET);
 #elif
-    fprintf(stderr, "\nError: %s | matrix.c on line %d\n", m, line);
+	fprintf(stderr, "\nError: %s | matrix.c on line %d\n", m, line);
 #endif // __linux__
 }
 
@@ -55,177 +55,176 @@ static void pferror(const char *m, const int line) {
  */
 static void pfinfo(const char *m) {
 #ifdef __linux__
-    fprintf(stderr, "\n%sInfo: %s%s", ANSI_COLOR_GREEN, m, ANSI_COLOR_RESET);
+	fprintf(stderr, "\n%sInfo: %s%s", ANSI_COLOR_GREEN, m, ANSI_COLOR_RESET);
 #elif
-    fprintf(stderr, "\nInfo: %s", m);
+	fprintf(stderr, "\nInfo: %s", m);
 #endif // __linux__
 }
 
 Matrix *matrix_init(int x, int y) {
-    Matrix *m = (Matrix*) malloc(sizeof(Matrix*));
+	Matrix *m = (Matrix*) malloc(sizeof(Matrix*));
 
-    if (check_pointer(m)) {
-        pferror("Couldn't initialise the Matrix, exiting.", __LINE__);
-        exit(1);
-    }
+	if (check_pointer(m)) {
+		pferror("Couldn't initialise the Matrix, exiting.", __LINE__);
+		exit(1);
+	}
 
-    // Set to be used in other functions
-    rows = x;
-    columns = y;
+	// Set to be used in other functions
+	rows = x;
+	columns = y;
 
-    // The process of creating the Matrix is create all the lines of the list
-    // first and then create a double linked list and for each pair of lists
-    // concatenate (link) those sublist together.
+	// The process of creating the Matrix is create all the lines of the list
+	// first and then create a double linked list and for each pair of lists
+	// concatenate (link) those sublist together.
 
-    // Walks along the HEADs.
-    MatrixNode* walker = NULL;
+	// Walks along the HEADs.
+	MatrixNode* walker = NULL;
 
-    // Create every HEAD of the Matrix
-    for (int i = 0; i < x; i++) {
-        // Creating the HEAD node of the Matrix
-        MatrixNode *HEAD = malloc(sizeof(MatrixNode));
+	// Create every HEAD of the Matrix
+	for (int i = 0; i < x; i++) {
+		// Creating the HEAD node of the Matrix
+		MatrixNode *HEAD = malloc(sizeof(MatrixNode));
 
-        if (check_pointer(HEAD)) {
-            pferror("Creating a MatrixNode failed, no memory?", __LINE__);
-            exit(1);
-        }
+		if (check_pointer(HEAD)) {
+			pferror("Creating a MatrixNode failed, no memory?", __LINE__);
+			exit(1);
+		}
 
-        // Setting its position on the Matrix
-        HEAD->pos_x = i;
-        // The HEAD element is always the first of its column
-        HEAD->pos_y = 0;
+		// Setting its position on the Matrix
+		HEAD->pos_x = i;
+		// The HEAD element is always the first of its column
+		HEAD->pos_y = 0;
 
-        // Only for testing purposes
-        HEAD->value = i;
+		// Only for testing purposes
+		HEAD->value = i;
 
-        if (*m == NULL) {
-            // Empty Matrix
-            HEAD->top = HEAD->bottom = NULL;
-            *m = HEAD;
-        } else {
-            walker->bottom = HEAD;
-            HEAD->top = walker;
-        }
+		if (*m == NULL) {
+			// Empty Matrix
+			HEAD->top = HEAD->bottom = NULL;
+			*m = HEAD;
+		} else {
+			walker->bottom = HEAD;
+			HEAD->top = walker;
+		}
 
-        HEAD->left = HEAD->right = NULL;
+		HEAD->left = HEAD->right = NULL;
 
-        walker = HEAD;
-
-#ifdef DEBUG
-        const char *message = "Creating HEAD %d. Walker %p |\
- Walker's top is %p";
-        int size = snprintf(NULL, 0, message, i, walker, walker->top);
-        char buffer[size+1];
-        snprintf(buffer, size, message, i, walker, walker->top);
-        pfinfo(buffer);
-#endif // DEBUG
-    }
-
-    // Creating every line for each HEAD.
-    for (int i = 0; i < x; i++) {
-        MatrixNode* HEAD = NULL;
-        if (i == 0) {
-            // Points to the first position of the Matrix available
-
-            // FIXME: This looks like Macgyver, need to revisit this later.
-            while (walker->top != NULL) {
-                walker = walker->top;
-            }
-
-            HEAD = walker;
-        } else {
-            HEAD = walker->bottom;
-            walker = walker->bottom;
-
-            HEAD = walker;
-        }
-
-        for (int j = 1; j < y; j++) {
-            MatrixNode *e = malloc(sizeof(MatrixNode));
-
-            if (check_pointer(e)) {
-                pferror("Creating a MatrixNode failed, no memory?", __LINE__);
-                exit(1);
-            }
-
-            HEAD->right = e;
-
-            e->left = HEAD;
-            e->bottom = e->top = NULL;
-            // not sure if the below will work well
-            e->pos_x = i;
-            e->pos_y = j;
-            e->value = i+j;
-
+		walker = HEAD;
 
 #ifdef DEBUG
-            const char *message = "Creating element %d. Row walker %p |\
- Row walker's left is %p";
-            int size = snprintf(NULL, 0, message, j, HEAD, HEAD->left);
-            char buffer[size+1];
-            snprintf(buffer, size, message, j, HEAD, HEAD->left);
-            pfinfo(buffer);
+		const char *message = "Creating HEAD %d. Walker %p |\
+		Walker's top is %p";
+		int size = snprintf(NULL, 0, message, i, walker, walker->top);
+		char buffer[size+1];
+		snprintf(buffer, size, message, i, walker, walker->top);
+		pfinfo(buffer);
+#endif // DEBUG
+	}
+
+	// Creating every line for each HEAD.
+	for (int i = 0; i < x; i++) {
+		MatrixNode* HEAD = NULL;
+		if (i == 0) {
+			// Points to the first position of the Matrix available
+
+			// FIXME: This looks like Macgyver, need to revisit this later.
+			while (walker->top != NULL) {
+				walker = walker->top;
+			}
+
+			HEAD = walker;
+		} else {
+			HEAD = walker->bottom;
+			walker = walker->bottom;
+
+			HEAD = walker;
+		}
+
+		for (int j = 1; j < y; j++) {
+			MatrixNode *e = malloc(sizeof(MatrixNode));
+
+			if (check_pointer(e)) {
+				pferror("Creating a MatrixNode failed, no memory?", __LINE__);
+				exit(1);
+			}
+
+			HEAD->right = e;
+
+			e->left = HEAD;
+			e->bottom = e->top = NULL;
+			// not sure if the below will work well
+			e->pos_x = i;
+			e->pos_y = j;
+			e->value = i+j;
+
+
+#ifdef DEBUG
+			const char *message = "Creating element %d. Row walker %p |\
+			Row walker's left is %p";
+			int size = snprintf(NULL, 0, message, j, HEAD, HEAD->left);
+			char buffer[size+1];
+			snprintf(buffer, size, message, j, HEAD, HEAD->left);
+			pfinfo(buffer);
 #endif // DEBUG
 
-            HEAD = e;
-        }
-    }
+			HEAD = e;
+		}
+	}
 
-    MatrixNode *row_walker = *m;
+	MatrixNode *row_walker = *m;
 
-    // Concatenation of every row of the Matrix
-    for (int i = 0; i < x; i++) {
-        MatrixNode *row = row_walker;
-        MatrixNode *row_next = row_walker->bottom;
+	// Concatenation of every row of the Matrix
+	for (int i = 0; i < x; i++) {
+		MatrixNode *row = row_walker;
+		MatrixNode *row_next = row_walker->bottom;
 
-        if (row_next == NULL) {
-            break;
-        }
+		if (row_next == NULL) {
+			break;
+		}
 
-        // Move one column right
-        row = row->right;
-        row_next = row_next->right;
+		// Move one column right
+		row = row->right;
+		row_next = row_next->right;
 
-        for (int j = 0; j < y; j++) {
-            row->bottom = row_next;
-            row_next->top = row;
+		for (int j = 0; j < y; j++) {
+			row->bottom = row_next;
+			row_next->top = row;
 
-            // It only needs one verification
-            if (row->right != NULL) {
-                row = row->right;
-                row_next = row_next->right;
-            }
-        }
+			// It only needs one verification
+			if (row->right != NULL) {
+				row = row->right;
+				row_next = row_next->right;
+			}
+		}
 
-        row_walker = row_walker->bottom;
-    }
+		row_walker = row_walker->bottom;
+	}
 
 
-    return m;
+	return m;
 }
 
 int matrix_insert(Matrix *m, uint32_t x, uint32_t y, int v) {
-	 if (check_pointer(m)) {
-        pferror("Argument given to matrix_insert is null",
-                __LINE__);
-        return NULL;
-    } else if (x >= rows || y >= columns) {
-        pferror("Values given are out of bounds", __LINE__);
-        return NULL;
-    }
+	if (check_pointer(m)) {
+		pferror("Argument given to matrix_insert is null",
+			__LINE__);
+		return NULL;
+	} else if (x >= rows || y >= columns) {
+		pferror("Values given are out of bounds", __LINE__);
+		return NULL;
+	}
 
-    MatrixNode *walker = *m;
+	MatrixNode *walker = *m;
 
-    while(walker->pos_x != x && walker->bottom != NULL){
-    	walker = walker->bottom;
-    }
+	while(walker->pos_x != x && walker->bottom != NULL){
+		walker = walker->bottom;
+	}
 
-    while(walker->pos_y != y && walker->right != NULL){
-    	walker = walker->right;
-    }
+	while(walker->pos_y != y && walker->right != NULL){
+		walker = walker->right;
+	}
 
-   
-    	walker->value = v;
+	walker->value = v;
 }
 
 int matrix_remove_by_coordinate(Matrix *m, uint32_t x, uint32_t y) {
@@ -246,20 +245,24 @@ int matrix_remove_by_coordinate(Matrix *m, uint32_t x, uint32_t y) {
 		walker = walker->right;
 	}
 
-    if (check_pointer(walker)) {
-    	pferror("Unable to find this coordinate", __LINE__);
-    }
-    else {
-    	if (walker->top != NULL) 
-    		walker->top->bottom = walker->bottom;
-    	if (walker->bottom != NULL) 
-    		walker->bottom->top = walker->top;
-    	if (walker->left != NULL)
-    		walker->left->right = walker->right;
-    	if (walker->right != NULL)
-    		walker->right->left = walker->left;
-    	free(walker);
-    }
+	if (check_pointer(walker)) {
+		pferror("Unable to find this coordinate", __LINE__);
+	}
+	else {
+		if (walker->top != NULL) {
+			walker->top->bottom = walker->bottom;
+		}
+		if (walker->bottom != NULL) {
+			walker->bottom->top = walker->top;
+		}
+		if (walker->left != NULL) {
+			walker->left->right = walker->right;
+		}
+		if (walker->right != NULL) {
+			walker->right->left = walker->left;
+		}
+		free(walker);
+	}
 }
 
 int matrix_remove_by_value(Matrix *m, int v) {
@@ -274,15 +277,19 @@ int matrix_remove_by_value(Matrix *m, int v) {
 		walker = HEAD;
 		while (walker->right != NULL) {
 			if (walker->value == v){
-				if (walker->top != NULL) 
-		    		walker->top->bottom = walker->bottom;
-		    	if (walker->bottom != NULL) 
-		    		walker->bottom->top = walker->top;
-		    	if (walker->left != NULL)
-		    		walker->left->right = walker->right;
-		    	if (walker->right != NULL)
-		    		walker->right->left = walker->left;
-		    	free(walker);
+				if (walker->top != NULL) {
+					walker->top->bottom = walker->bottom;
+				}
+				if (walker->bottom != NULL) {
+					walker->bottom->top = walker->top;
+				}
+				if (walker->left != NULL) {
+					walker->left->right = walker->right;
+				}
+				if (walker->right != NULL) {
+					walker->right->left = walker->left;
+				}
+				free(walker);
 			}
 			walker = walker->right;
 		}
@@ -291,183 +298,183 @@ int matrix_remove_by_value(Matrix *m, int v) {
 }
 
 MatrixNode *matrix_get_by_coordinate(MatrixNode *m, uint32_t x, uint32_t y) {
-    if (check_pointer(m)) {
-        pferror("Argument given to matrix_get_by_coordinate is null",
-                __LINE__);
-        return NULL;
-    } else if (x >= rows || y >= columns) {
-        pferror("Values given are out of bounds", __LINE__);
-        return NULL;
-    }
+	if (check_pointer(m)) {
+		pferror("Argument given to matrix_get_by_coordinate is null",
+			__LINE__);
+		return NULL;
+	} else if (x >= rows || y >= columns) {
+		pferror("Values given are out of bounds", __LINE__);
+		return NULL;
+	}
 
-    MatrixNode *desired = m;
+	MatrixNode *desired = m;
 
-    while (desired->pos_x != x) {
-        desired = desired->bottom;
-    }
+	while (desired->pos_x != x) {
+		desired = desired->bottom;
+	}
 
-    while (desired->pos_y != y) {
-        desired = desired->right;
-    }
+	while (desired->pos_y != y) {
+		desired = desired->right;
+	}
 
-    return desired;
+	return desired;
 }
 
 MatrixNode *matrix_get_by_value(MatrixNode *m, int v) {
-    if (check_pointer(m)) {
-        pferror("Argument given to matrix_get_by_value is null", __LINE__);
-        return NULL;
-    }
+	if (check_pointer(m)) {
+		pferror("Argument given to matrix_get_by_value is null", __LINE__);
+		return NULL;
+	}
 
-    MatrixNode *desired = NULL;
+	MatrixNode *desired = NULL;
 
-    while (m->bottom != NULL) {
-        while (m->right != NULL) {
-            if (m->value == v) {
-                desired = m;
-                // While some might say that a goto is not a ideal solution
-                // but it is, and here's why:
-                // Otherwise to break a nested loop I'd have to use flags,
-                // they would cost a little bit of memory which I wouldn't
-                // like to spare. Further on unconditional jmps are
-                // everywhere in machine code, I'm just exploring it on this
-                // level.
-                goto end;
-            }
-            m = m->right;
-        }
+	while (m->bottom != NULL) {
+		while (m->right != NULL) {
+			if (m->value == v) {
+				desired = m;
+				// While some might say that a goto is not a ideal solution
+				// but it is, and here's why:
+				// Otherwise to break a nested loop I'd have to use flags,
+				// they would cost a little bit of memory which I wouldn't
+				// like to spare. Further on unconditional jmps are
+				// everywhere in machine code, I'm just exploring it on this
+				// level.
+				goto end;
+			}
+			m = m->right;
+		}
 
-        m = m->bottom;
-    }
+		m = m->bottom;
+	}
 
-end:
-    return desired;
+	end:
+	return desired;
 }
 
 int *matrix_get_value(MatrixNode *m) {
-    if (check_pointer(m)) {
-        pferror("MatrixNode given is null", __LINE__);
-        return NULL;
-    }
+	if (check_pointer(m)) {
+		pferror("MatrixNode given is null", __LINE__);
+		return NULL;
+	}
 
-    return &(m->value);
+	return &(m->value);
 }
 
 int matrix_print(Matrix m) {
-    if (check_pointer(m)) {
-        pferror("Argument given to matrix_print is null", __LINE__);
-        return 0;
-    }
+	if (check_pointer(m)) {
+		pferror("Argument given to matrix_print is null", __LINE__);
+		return 0;
+	}
 
-    MatrixNode *HEAD = m;
+	MatrixNode *HEAD = m;
 
-    newline;
+	newline;
 
-    while (HEAD != NULL) {
-        m = HEAD;
-        while (m != NULL) {
-            printf(" -- [%d (%d,%d)]", m->value, m->pos_x, m->pos_y);
+	while (HEAD != NULL) {
+		m = HEAD;
+		while (m != NULL) {
+			printf(" -- [%d (%d,%d)]", m->value, m->pos_x, m->pos_y);
 
-            // Rightmost column of the matrix.
-            if (m->right == NULL) {
-                printf(" --");
-            }
+			// Rightmost column of the matrix.
+			if (m->right == NULL) {
+				printf(" --");
+			}
 
-            m = m->right;
-        }
-        newline;
+			m = m->right;
+		}
+		newline;
 
-        HEAD = HEAD->bottom;
-    }
+		HEAD = HEAD->bottom;
+	}
 
-    return 1;
+	return 1;
 }
 
 void matrix_print_heads(MatrixNode *m) {
-    if (check_pointer(m)) {
-        return;
-    }
+	if (check_pointer(m)) {
+		return;
+	}
 
-    printf("\n\t--(%d)--\n\t%4s", m->value, "|");
-    matrix_print_heads(m->bottom);
+	printf("\n\t--(%d)--\n\t%4s", m->value, "|");
+	matrix_print_heads(m->bottom);
 }
 
 int matrix_print_element(MatrixNode *m) {
-    if (m == NULL) {
-        pferror("Argument given to matrix_print_element is null", __LINE__);
-        return 0;
-    }
+	if (m == NULL) {
+		pferror("Argument given to matrix_print_element is null", __LINE__);
+		return 0;
+	}
 
-    printf("\n[%d (%d,%d)]", m->value, m->pos_x, m->pos_y);
+	printf("\n[%d (%d,%d)]", m->value, m->pos_x, m->pos_y);
 
-    return 1;
+	return 1;
 }
 
 int matrix_print_neighbours(MatrixNode *m) {
-    if (check_pointer(m)) {
-        pferror("Argument given to matrix_print_neighbours is null",
-                __LINE__);
-        return 0;
-    }
+	if (check_pointer(m)) {
+		pferror("Argument given to matrix_print_neighbours is null",
+			__LINE__);
+		return 0;
+	}
 
 
-    // Should minimise this code using some sort of generalization
-    if (!check_pointer(m->top)) {
-        printf("\t\t  [%d (%d,%d)]\n", (m->top)->value, (m->top)->pos_x,
-               (m->top)->pos_y);
-    }  else {
-        printf("\t\t    %sNULL%s\n", ANSI_COLOR_RED, ANSI_COLOR_RESET);
-    }
+	// Should minimise this code using some sort of generalization
+	if (!check_pointer(m->top)) {
+		printf("\t\t  [%d (%d,%d)]\n", (m->top)->value, (m->top)->pos_x,
+			(m->top)->pos_y);
+	}  else {
+		printf("\t\t    %sNULL%s\n", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+	}
 
 
-    if (!check_pointer(m->left)) {
-        printf("\t[%d (%d,%d)] ", (m->left)->value, (m->left)->pos_x,
-               (m->left)->pos_y);
-    } else {
-        printf("\t     %sNULL%s ", ANSI_COLOR_RED, ANSI_COLOR_RESET);
-    }
+	if (!check_pointer(m->left)) {
+		printf("\t[%d (%d,%d)] ", (m->left)->value, (m->left)->pos_x,
+			(m->left)->pos_y);
+	} else {
+		printf("\t     %sNULL%s ", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+	}
 
-    printf("%s[%d (%d,%d)]%s", ANSI_COLOR_GREEN, m->value, m->pos_x,
-           m->pos_y, ANSI_COLOR_RESET);
+	printf("%s[%d (%d,%d)]%s", ANSI_COLOR_GREEN, m->value, m->pos_x,
+		m->pos_y, ANSI_COLOR_RESET);
 
-    if (!check_pointer(m->right)) {
-        printf(" [%d (%d,%d)]", (m->right)->value, (m->right)->pos_x,
-               (m->right)->pos_y);
-    } else {
-        printf(" %sNULL%s", ANSI_COLOR_RED, ANSI_COLOR_RESET);
-    }
+	if (!check_pointer(m->right)) {
+		printf(" [%d (%d,%d)]", (m->right)->value, (m->right)->pos_x,
+			(m->right)->pos_y);
+	} else {
+		printf(" %sNULL%s", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+	}
 
 
-    if (!check_pointer(m->bottom)) {
-        printf("\n\t\t  [%d (%d,%d)]", (m->bottom)->value,
-               (m->bottom)->pos_x, (m->bottom)->pos_y);
-    }  else {
-        printf("\n\t\t    %sNULL%s", ANSI_COLOR_RED, ANSI_COLOR_RESET);
-    }
+	if (!check_pointer(m->bottom)) {
+		printf("\n\t\t  [%d (%d,%d)]", (m->bottom)->value,
+			(m->bottom)->pos_x, (m->bottom)->pos_y);
+	}  else {
+		printf("\n\t\t    %sNULL%s", ANSI_COLOR_RED, ANSI_COLOR_RESET);
+	}
 
-    newline;
-    newline;
+	newline;
+	newline;
 
-    return 1;
+	return 1;
 }
 
 void matrix_free(MatrixNode *m) {
-    //The free method is described in the Free section of the paper.
-    MatrixNode *walker = m;
+	//The free method is described in the Free section of the paper.
+	MatrixNode *walker = m;
 
-    // Walker starts at 0x0 if not we have to reset its position.
-    while(walker != NULL) {
-        m = walker;
+	// Walker starts at 0x0 if not we have to reset its position.
+	while(walker != NULL) {
+		m = walker;
 
-        while (m != NULL) {
-            MatrixNode *e = m;
-            m = m->right;
+		while (m != NULL) {
+			MatrixNode *e = m;
+			m = m->right;
 
-            free(e);
-        }
+			free(e);
+		}
 
-        walker = walker->bottom;
-    }
+		walker = walker->bottom;
+	}
 
-    free(m);
+	free(m);
 }
