fd4ea1309b09485a245c967e63c90321b7a41fd5
Rafael Campos Nunes <rafaelnunes@engineer.com>
2017-10-20 04:03:00 -0200

    Adds the statically circular queue implementation
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ src/statically-allocated/circular-queue/circular_queue.c
@@ -0,0 +1,86 @@
+#include "circular_queue.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief The structure that defines a circular queue.
+ */
+struct cqueue {
+    ///
+    int head;
+    ///
+    int tail;
+    ///
+    int data[CQUEUE_SIZE];
+};
+
+
+static void pferror(const char* m, int l) {
+    fprintf(stderr, "\n\n--> %s | on %s line %d", m,  __FILE__, l);
+}
+
+CircularQueue* queue_init() {
+    CircularQueue* cq = malloc(sizeof(CircularQueue));
+
+    if (check_pointer(cq)) {
+        pferror("Could not allocate memory for circular queue.", __LINE__);
+        exit(1);
+    }
+
+    return cq;
+}
+
+int queue_push(CircularQueue* cq, int v) {
+    if (check_pointer(cq)) {
+        pferror("Circular queue given to push is null", __LINE__);
+        return 0;
+    }
+
+    cq->tail += 1 % CQUEUE_SIZE;
+    cq->data[cq->tail] = v;
+
+    return 1;
+}
+
+Value queue_pop(CircularQueue* cq) {
+    Value v;
+
+    v.v = NULL;
+
+    if (check_pointer(cq)) {
+        pferror("Circular queue given to pop is null", __LINE__);
+        return v;
+    }
+
+    if (queue_empty(cq)) {
+        pferror("Circular queue is empty", __LINE__);
+        return v;
+    }
+
+    v.v = &cq->data[cq->head];
+    cq->head += 1 % CQUEUE_SIZE;
+
+    return v;
+}
+
+int queue_empty(CircularQueue *cq) {
+    // not sure if entirely complete/correct
+    return queue_size(cq) == 0;
+}
+
+int queue_full(CircularQueue *cq) {
+    return queue_size(cq) == CQUEUE_SIZE;
+}
+
+int queue_size(CircularQueue *cq) {
+    return (CQUEUE_SIZE - cq->head + cq->tail) % CQUEUE_SIZE;
+}
+
+int check_pointer(void *p) {
+    if (p != NULL) {
+        return 1;
+    }
+
+    return 0;
+}
---
+++ src/statically-allocated/circular-queue/circular_queue.h
@@ -0,0 +1,54 @@
+#ifndef CQUEUE_H
+#define CQUEUE_H
+
+#define CQUEUE_SIZE 10
+
+/**
+ * A structure to encapsulate the ~optional~ return condition, the value
+ * returned by a function might be NULL or not.
+ */
+struct value {
+    int *v;
+};
+
+typedef struct value Value;
+
+typedef struct cqueue CircularQueue;
+
+/**
+ * @brief
+ */
+CircularQueue* queue_init();
+
+/**
+ * @brief
+ */
+int queue_push(CircularQueue*, int);
+
+/**
+ * @brief
+ */
+Value queue_pop(CircularQueue*);
+
+/**
+ * @brief
+ */
+int queue_empty(CircularQueue*);
+
+/**
+ * @brief
+ */
+int queue_full(CircularQueue*);
+
+/**
+ * @brief
+ */
+int queue_size(CircularQueue*);
+
+/**
+ * @brief
+ */
+int check_pointer(void *);
+
+
+#endif // CQUEUE_H
