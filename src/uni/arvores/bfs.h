883f75e256bfc5797ce5467ca3ff8bcdd8618714
Rafael Campos Nunes <rafaelnunes@engineer.com>
2017-11-15 01:02:53 -0200

    Adds utterly incomplete shit to the new BFS
        Initial commit to allow Mikael work on it as well.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ src/uni/arvores/bfs.h
@@ -0,0 +1,12 @@
+#ifndef BFS_H
+#define BFS_H
+
+#include <stdlib.h>
+
+typedef struct filesystem Filesystem;
+
+void
+bfs_load(FILE* virtual_filesystem);
+
+
+#endif // BFS_H
---
+++ src/uni/arvores/cmdline.h
@@ -0,0 +1,6 @@
+#ifndef CMDLINE_H
+#define CMDLINE_H
+
+void cmdline_start();
+
+#endif // CMDLINE_H
