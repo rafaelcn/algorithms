2e07d467e69bdf81b44a0cbdfb20d7c78b201da0
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-04-12 23:10:06 -0300

    Add bubble and selection sort implementation
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ src/sort algorithms/bubble/bubble.cpp
@@ -0,0 +1,58 @@
+/** MIT License
+ *
+ * Copyright (c) 2017-2018 Rafael C. Nunes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void bubble_sort(std::vector<T> &);
+
+int main() {
+
+    std::vector<int> instance = {9, 3, 1, 4, 5, 10, 2, 8, 6, 7};
+
+    std::cout << "Unsorted instance: \t";
+    for (const auto e: instance) {
+        std::cout << e << " ";
+    }
+    std::cout << "\n";
+
+    bubble_sort(instance);
+
+    std::cout << "Sorted instance: \t";
+    for (const auto e: instance) {
+        std::cout << e << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
+
+template <typename T>
+void bubble_sort(std::vector<T> &instance) {
+
+    for (size_t i = 0; i < instance.size(); i++) {
+        for (size_t j = 1; j < instance.size(); j++) {
+            if (instance[j] < instance[j-1]) {
+                std::swap(instance[j], instance[j-1]);
+            }
+        }
+    }
+}
