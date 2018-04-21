83c2e522b01e902b33db5619158abae680f8090c
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-04-21 18:48:48 -0300

    Add the insertion sort
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ src/sort algorithms/insertion/insertion.cpp
@@ -0,0 +1,67 @@
+/** MIT License
+ *
+ * Copyright (c) 2017-2018 Rafael C. Nunes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void insertion(std::vector<T> &);
+
+int main() {
+
+    std::vector<int> instance = {9, 3, 1, 4, 5, 10, 2, 8, 6, 7};
+
+    std::cout << "Unsorted instance: ";
+
+    for (auto e : instance) {
+        std::cout << e << " ";
+    }
+    std::cout << "\n";
+
+    insertion(instance);
+
+    std::cout << "Sorted instance:   ";
+
+    for (auto e : instance) {
+        std::cout << e << " ";
+    }
+    std::cout << "\n";
+
+
+
+    return 0;
+}
+
+template <typename T>
+void insertion(std::vector<T> &instance) {
+
+    for (size_t i = 1; i < instance.size(); i++) {
+        T aux = instance[i];
+        size_t j = i-1;
+
+
+        while (j >= 0 && instance[j] > aux) {
+            instance[j+1] = instance[j];
+            j--;
+        }
+
+        instance[j+1] = aux;
+    }
+}
