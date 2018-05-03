1c1e1eef3945d896cffbd62aca081636912f6905
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-05-03 13:32:36 -0300

    Fix bubble sort
        The algorithm was not behaving O(n) in the best case and then I
    realized that it was missing something. The swapped variable fix that.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/sort algorithms/bubble/bubble.h
+++ src/sort algorithms/bubble/bubble.h
@@ -2,14 +2,20 @@
 #define BUBBLE_H
 
 #include <vector>
+#include <cstdio>
 
 template <typename T>
 void bubble_sort(std::vector<T> &instance) {
 
     for (std::size_t i = 0; i < instance.size(); i++) {
-        for (std::size_t j = 1; j < instance.size(); j++) {
+        bool swapped = true;
+
+        for (std::size_t j = 1; j < instance.size() && swapped; j++) {
+            swapped = false;
+
             if (instance[j] < instance[j-1]) {
                 std::swap(instance[j], instance[j-1]);
+                swapped = true;
             }
         }
     }
