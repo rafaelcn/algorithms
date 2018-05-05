f593eca301078610e259a65ce55c50fa9510d244
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-05-05 13:09:35 -0300

    Add the heap sort algorithm
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ src/sort algorithms/heap/heap.cpp
@@ -0,0 +1,59 @@
+/** MIT License
+ *
+ * Copyright (c) 2017-2018 Rafael C. Nunes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "heap.h"
+
+
+void heapify(std::vector<int> &v, int n, int i) {
+
+    int largest = i;
+
+    // position of the sons of the node at v[i]
+    int l = 2*i+1;
+    int r = 2*(i+1);
+
+    if (l < n && v[l] > v[largest]) {
+        largest = l;
+    }
+
+    if (r < n && v[r] > v[largest]) {
+        largest = r;
+    }
+
+    if (largest != i) {
+        std::swap(v[i], v[largest]);
+        heapify(v, n, largest);
+    }
+}
+
+void heap(std::vector<int> &v, int n) {
+
+    // build heap
+    for (int i = n/2-1; i >= 0; i--) {
+        heapify(v, n, i);
+    }
+
+    // sort
+    for (int i = n - 1; i >= 0; i--) {
+        std::swap(v[0], v[i]);
+        // update the heap every time you change the original instance
+        heapify(v, i, 0);
+    }
+}
---
+++ src/sort algorithms/heap/main.cpp
@@ -0,0 +1,54 @@
+/** MIT License
+ *
+ * Copyright (c) 2017-2018 Rafael C. Nunes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+#include <iostream>
+#include <vector>
+
+#include "heap.h"
+
+int main() {
+
+    std::vector<int> instance;
+    instance.resize(1000);
+
+    int j = instance.size();
+    int i;
+
+    // filling the instance
+    for (i = 0; i < instance.size(); i++) {
+        instance[i] = j;
+        j--;
+    }
+
+    for (i = 0; i < instance.size(); i++) {
+        std::cout << instance[i] << " ";
+    }
+    std::cout << "\n";
+
+    heap(instance, instance.size());
+
+    for (i = 0; i < instance.size(); i++) {
+        std::cout << instance[i] << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
